e8898f5a2b37166261384913c41f11e226b28bfd
Ercogx <ercogx@gmail.com>
2025-03-13 23:06:46 +0200

    Configure app service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,23 +2,53 @@
 
 namespace App\Providers;
 
+use BezhanSalleh\FilamentShield\FilamentShield;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
+    protected array $policies = [];
+
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        $this->configurePolicies();
+
+        $this->configureDB();
+
+        $this->configureModels();
+
+        $this->configureFilament();
+    }
+
+    private function configurePolicies(): void
+    {
+        foreach ($this->policies as $model => $policy) {
+            Gate::policy($model, $policy);
+        }
+    }
+
+    private function configureDB(): void
+    {
+        DB::prohibitDestructiveCommands($this->app->environment('production'));
+    }
+
+    private function configureModels(): void
+    {
+        Model::preventAccessingMissingAttributes();
+
+        Model::unguard();
+    }
+
+    private function configureFilament(): void
+    {
+        FilamentShield::prohibitDestructiveCommands($this->app->isProduction());
     }
 }
